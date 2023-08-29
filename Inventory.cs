d4a968e5671f4fa8b86b74ea288263e47bce52ba
LeeHaTaeg <gkxor6457@gmail.com>
2023-08-29 12:20:24 +0900

    invenworking2

--- Inventory.cs
+++ Inventory.cs
@@ -1,19 +1,30 @@
+using SprtaGame;
 using System.Runtime.InteropServices.JavaScript;
 
 public class Inventory
 {
-    List<Item> playerInven = new List<Item>();
+    List<Item> playerInven = new List<Item>(10);
     //아래는 인벤토리 리스트에서 각각 무기와 갑옷의 인덱스. 어떻게 고칠지 모르겠다.
     public int exArmorNum = -1;
     public int exWeaponNum = -1;
+    Player player = Program.defaultPlayer;
     //스테이터스
     public Inventory()
     {
         Item steelArmor = new Item("무쇠갑옷", 0, 5, 500, "무쇠로 만들어져 튼튼한 갑옷입니다.");
+        Item leatherArmor = new Item("가죽갑옷", 0, 2, 200, "소가죽으로 만들어진 낡은 가죽갑옷.");
+
         Item oldSword = new Item("낡은 검", 2, 0, 800, "쉽게 볼 수 있는 낡은 검입니다.");
+        Item gladius = new Item("글라디우스", 6, 0, 1000, "찌르기에 특화된 사정거리 짧은 한손검.");
+
+        //Item lowHpPotion = new Potion("하급 체력 포션", 0, 0, 400, 20,  "초보 모험자들이 애용하는 물약. 치유량은 미미하다.");
+        //Item lowAtkPotion = new Potion("하급 힘 포션", 2, 0, 300, 0,  "저레벨 몬스터들을 재료로 고아낸 힘 물약.");
+        //Item lowDefPotion = new Potion("하급 방어력 포션", 0, 2, 200, 0,  "일부 몬스터들의 갑각을 재료로 고아낸 물약");
 
         playerInven.Add(steelArmor);
+        playerInven.Add(leatherArmor);
         playerInven.Add(oldSword);
+        playerInven.Add(gladius);
     }
     public void DisplayInven()
     {
@@ -40,19 +51,19 @@ public class Inventory
             Replace_Name = InvenStr_Name.Remove(0, nameLength)
                                         .Insert(0, playerInven[currentIndex].item_Name);
 
-            if (playerInven[currentIndex].item_Atk == 0)
+            if (playerInven[currentIndex].item_Damage == 0)
             {
                 //armor
-                int effectLength = HowManyDigit(playerInven[currentIndex].item_Def) + 6;
+                int effectLength = HowManyDigit(playerInven[currentIndex].item_Defence) + 6;
                 Replace_Effect = InvenStr_Effect.Remove(0, effectLength)
-                                                .Insert(0, " 방어력 +" + Convert.ToString(playerInven[currentIndex].item_Def));
+                                                .Insert(0, " 방어력 +" + Convert.ToString(playerInven[currentIndex].item_Defence));
             }
             else
             {
                 //weapon
-                int effectLength = HowManyDigit(playerInven[currentIndex].item_Atk) + 6;
+                int effectLength = HowManyDigit(playerInven[currentIndex].item_Damage) + 6;
                 Replace_Effect = InvenStr_Effect.Remove(0, effectLength)
-                                                .Insert(0, " 공격력 +" + Convert.ToString(playerInven[currentIndex].item_Atk));
+                                                .Insert(0, " 공격력 +" + Convert.ToString(playerInven[currentIndex].item_Damage));
             }
             int explainLength = playerInven[currentIndex].item_Discription.Length;
             Replace_Explain = InvenStr_Explain.Remove(0, explainLength)
@@ -68,14 +79,143 @@ public class Inventory
         Console.WriteLine(". 나가기");
         ConsoleUI.Write(ConsoleColor.DarkRed, "1");
         Console.WriteLine(". 장착관리");
+        Console.WriteLine();
+
+        Console.WriteLine("원하시는 행동을 입력해주세요.");
+        ConsoleUI.Write(ConsoleColor.Yellow, ">> ");
+
+        var currentCursor = Console.GetCursorPosition();
+        int inputNumber = 0;
+
+        while (isWrongInput)
+        {
+            if (int.TryParse(Console.ReadLine(), out inputNumber) == true)
+            {
+                if (inputNumber == 0 || inputNumber == 1)
+                    break;
+            }
+            Console.SetCursorPosition(currentCursor.Left, currentCursor.Top);
+            ConsoleUI.Write(ConsoleColor.Red, "잘못된 입력입니다.");
+            Thread.Sleep(1000);
+            Console.SetCursorPosition(currentCursor.Left, currentCursor.Top);
+            Console.Write("                    ");
+            Console.SetCursorPosition(currentCursor.Left, currentCursor.Top);
+        }
+
+        switch (inputNumber)
+        {
+            case 0:
+                MainPage.GameStart();
+                break;
+            case 1:
+                DisplayEquip();
+                break;
+        }
+    }
+    public void DisplayEquip()
+    {
+        Console.Clear();
+        Console.WriteLine("장착관리");
+
+        Console.WriteLine("아이템 번호를 입력하면 장착, 장착된 아이템 번호 입력 시 해제됩니다.");
+        Console.WriteLine();
+        Console.WriteLine("[아이템 목록]");
+
+        int invenLength = playerInven.Count();
+        int currentIndex = 0;
+
+        while (currentIndex < invenLength)
+        {
+            string InvenStr_Name = "          |";
+            string InvenStr_Effect = "          |";
+            string InvenStr_Explain = "                                                            |";
+            string Replace_Name = string.Empty;
+            string Replace_Effect = string.Empty;
+            string Replace_Explain = string.Empty;
+
+            int nameLength = playerInven[currentIndex].item_Name.Count();
+            Replace_Name = InvenStr_Name.Remove(0, nameLength)
+                                        .Insert(0, playerInven[currentIndex].item_Name);
+
+            if (playerInven[currentIndex].item_Damage == 0)
+            {
+                //armor
+                int effectLength = HowManyDigit(playerInven[currentIndex].item_Defence) + 6;
+                Replace_Effect = InvenStr_Effect.Remove(0, effectLength)
+                                                .Insert(0, " 방어력 +" + Convert.ToString(playerInven[currentIndex].item_Defence));
+            }
+            else
+            {
+                //weapon
+                int effectLength = HowManyDigit(playerInven[currentIndex].item_Damage) + 6;
+                Replace_Effect = InvenStr_Effect.Remove(0, effectLength)
+                                                .Insert(0, " 공격력 +" + Convert.ToString(playerInven[currentIndex].item_Damage));
+            }
+            int explainLength = playerInven[currentIndex].item_Discription.Length;
+            Replace_Explain = InvenStr_Explain.Remove(0, explainLength)
+                                              .Insert(0, playerInven[currentIndex].item_Discription);
+
+            ConsoleUI.Write(ConsoleColor DarkRed, currentIndex + 1);
+            Console.Write("- ");
+            Console.Write(Replace_Name);
+            Console.Write(Replace_Effect);
+            Console.Write(Replace_Explain);
+            currentIndex++;
+        }
+
+        ConsoleUI.Write(ConsoleColor.DarkRed, "0");
+        Console.WriteLine(". 나가기");
+        ConsoleUI.Write(ConsoleColor.DarkRed, "1~");
+        Console.WriteLine(". 장착/해제");
+        Console.WriteLine();
+
+        Console.WriteLine("원하시는 행동을 입력해주세요.");
+        ConsoleUI.Write(ConsoleColor.Yellow, ">> ");
+
+        var currentCursor = Console.GetCursorPosition();
+        int inputNumber = 0;
+
+        while (isWrongInput)
+        {
+            if (int.TryParse(Console.ReadLine(), out inputNumber) == true)
+            {
+                if ((inputNumber >= 0)&&(inputNumber <= playerInven.Count())
+                    break;
+            }
+            Console.SetCursorPosition(currentCursor.Left, currentCursor.Top);
+            ConsoleUI.Write(ConsoleColor.Red, "잘못된 입력입니다.");
+            Thread.Sleep(1000);
+            Console.SetCursorPosition(currentCursor.Left, currentCursor.Top);
+            Console.Write("                    ");
+            Console.SetCursorPosition(currentCursor.Left, currentCursor.Top);
+        }
+
+        if(inputNumber == 0)
+        {
+            DisplayInven();
+        }
+        else
+        {
+            Equip(inputNumber - 1);
+        }
     }
     public void InvenAdd(Item item)
     {
-        playerInven.Add(item);
+        if (playerInven.count == 10)
+        {
+            Console.WriteLine("인벤토리 창이 가득 찼습니다.");
+            Console.WriteLine("구입을 원하시면 인벤토리를 비워주십시오.");
+        }
+        else
+        {
+            playerInven.Add(item);
+        }
     }
-    public void InvenUse()
+    public void InvenUse(Item item)
     {
-
+        //포션류
+        player
+        playerInven.Remove(item);
     }
     public void Equip(int i)
     {
@@ -83,49 +223,50 @@ public class Inventory
 
         if (isEquiped == true)
         {
+            //player
             playerInven[i].item_Name = playerInven[i].item_Name.Replace("[E]", "");
 
             //if 해제
-            if (playerInven[i].item_Atk == 0)
+            if (playerInven[i].item_Damage == 0)
             {
                 //갑옷 해제
-                player.Def = player.Def - myInven[i].Item_Def;
+                player.Defence = player.Defence - myInven[i].Item_Defence;
             }
             else if (playerInven[i].item_Def == 0)
             {
             //무기 해제
-                player.Atk = player.Atk - playerInven[i].item_Atk;
+                player.Damage = player.Damage - playerInven[i].item_Damage;
             }
         }
         else
         {
             playerInven[i].item_Name = "[E]" + playerInven[i].item_Name;
             //갑옷장착
-            if (playerInven[i].item_Atk == 0)
+            if (playerInven[i].item_Damage == 0)
             {
                 //기존 장착한거 없으면
                 if(exArmorNum == -1)
                 {
-                    player.Def += playerInven[i].item_Def;
+                    player.Defence += playerInven[i].item_Defence;
                     exArmorNum = i;
                 }
                 //장착한게 있으면
                 else
                 {
-                    player.Def = player.Def - playerInven[exArmorNum].item_Def + playerInven[i].item_Def;
+                    player.Defence = player.Defence - playerInven[exArmorNum].item_Defence + playerInven[i].item_Defence;
                     exArmorNum = i;
                 }
             }
-            else if(playerInven[i].item_Def == 0)
+            else if(playerInven[i].item_Defence == 0)
             {
                 if(exWeaponNum == -1)
                 {
-                    player.Atk += playerInven[i].item_Atk;
+                    player.Damage += playerInven[i].item_Damage;
                     exWeaponNum = i;
                 }
                 else
                 {
-                    player.Atk = player.Atk - playerInven[exWeaponNum].item_Atk + playerInven[i].item_Atk;
+                    player.Damage = player.Damage - playerInven[exWeaponNum].item_Damage + playerInven[i].item_Damage;
                     exWeaponNum = i;
                 }
             }
--- Item.cs
+++ Item.cs
@@ -1,23 +1,34 @@
 public class Item
 {
-<<<<<<< HEAD
 
-    public string item_Name { get; set; }
-    public int item_Atk { get; set; }
-    public int item_Def { get; set; }
-    public int item_Gold { get; set; }
-    public string item_Discription { get; set; }
+    public string   item_Name { get; set; }
+    public int      item_Health { get; set; }
+    public int      item_Damage { get; set; }
+    public int      item_Defense { get; set; }
+    public int      item_Gold { get; set; }
+    public string   item_Discription { get; set; }
 
-    public Item(string _item_name, int _item_atk, int _item_def, int _item_Gold, string _item_discription)
+
+  
+
+    public Item(string _item_name, int _item_damage, int _item_defense, int _item_Gold, string _item_discription)
     {
+        //장비템
         item_Name = _item_name;
-        item_Atk = _item_atk;
-        item_Def = _item_def;
+        item_Damage = _item_damage;
+        item_Defense = _item_defense;
         item_Gold = _item_Gold;
         item_Discription = _item_discription;
     }
 
-=======
-    
->>>>>>> 7b330317c8aee14570acf9ada8de85c075321078
+    public Item(string _item_name, int _item_damage, int _item_defense, int _item_Gold, int _item_health, string _item_discription)
+    {
+        //물약류
+        item_Name = _item_name;
+        item_Health = _item_health;
+        item_Damage = _item_damage;
+        item_Defense = _item_defense;
+        item_Gold = _item_Gold;
+        item_Discription = _item_discription;
+    }
 }
